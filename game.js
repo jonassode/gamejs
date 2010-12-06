4c98a38a476fd1bfdf9de7fa59bcf65bcedceff3
Jonas Söderström <jonas.soderstrom@gmail.com>
2010-12-06 22:06:44 +0100

    Added two first functiosn"

---
+++ game.js
@@ -0,0 +1,14 @@
+
+// Builders
+// _scene
+
+
+function _scene( attributes ){
+    // Builds a Scene with the given attributes
+
+}
+
+function _( search_criteria ){
+    // Returns What Ever The Searc Criteria Found
+}
+
