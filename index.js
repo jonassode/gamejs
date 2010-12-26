d8ec46abcbc2a26df6f2e64ff43de6421bf434d1
Jonas Söderström <jonas.soderstrom@gmail.com>
2010-12-26 22:51:43 +0100

    Added third exampel node to index.js

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ window.onload = function () {
     Index.scene = _scene("exampel_canvas");
     _node({x:20,color:'silver'});
     _node({color:'pink'});
+    _node({y:40,width:5,height:20,x:20});
     Index.scene.draw();
 };
 
