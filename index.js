f88f3293c4123c47e314f8267bac778adfa99cf3
Jonas Söderström <jonas.soderstrom@gmail.com>
2010-12-27 12:05:47 +0100

    Added examples with transparent tress in layer

--- index.js
+++ index.js
@@ -9,7 +9,13 @@ window.onload = function () {
     Index.scene = _scene("exampel_canvas");
     Index.scene._layer("background");
     _node({img:'background.png'});
-    Index.scene._layer("default");
+
+    Index.scene._layer("trees");
+    _node({img:'tree.gif', y:110, x:20});
+    _node({img:'tree.gif', y:110, x:60});
+    _node({img:'tree.gif', y:110, x:80});
+   
+    Index.scene._layer("boxes");
     _node({x:20,color:'silver',width:10,height:10});
     _node({color:'pink',width:10,height:10});
     _node({y:40,width:5,height:20,x:20});
