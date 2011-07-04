a9a5764629db565586d89c8cca30dbd73b650fda
Jonas Söderström <jonas.soderstrom@gmail.com>
2011-07-04 20:54:10 +0200

    Added deploy script

---
+++ deploy.rb
@@ -0,0 +1,19 @@
+require 'fileutils'
+
+puts 'Deploying application ' + ARGV[0]
+
+t = Time.now()
+
+dir_root = "Deploys/" + ARGV[0] + t.strftime("%Y%m%d_%H%M")
+dir_ex = dir_root + "/Applications"
+Dir.mkdir(dir_root)
+Dir.mkdir(dir_ex)
+FileUtils.cp("game.js", dir_root)
+FileUtils.cp_r("Images", dir_root)
+FileUtils.cp_r('Applications/' + ARGV[0], dir_ex)
+
+puts dir_root
+
+
+
+
