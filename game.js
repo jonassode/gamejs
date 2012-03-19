47e20ef2c77aebb4101df5c8e096064b8ad4f45c
Jonas Söderström <jonas.soderstrom@gmail.com>
2012-03-19 21:35:38 +0100

    Removed redrawing of tilemap on tile.move, user should controll when tilemap is reloaded

--- game.js
+++ game.js
@@ -30,7 +30,7 @@ var _first_screen = null;
  * @namespace GAMEJS.Alpha
  */
 GAMEJS.Alpha = {}
-GAMEJS.Alpha.alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ:!.,1234567890-_'\"";
+GAMEJS.Alpha.alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ:!.,1234567890-_'?/[]()\"";
 GAMEJS.Alpha.letters = {};
 GAMEJS.Alpha.letter_padding = 2;
 GAMEJS.Alpha.letter_width = 5 + GAMEJS.Alpha.letter_padding;
@@ -46,13 +46,26 @@ GAMEJS.Alpha.letter_height = 8 + GAMEJS.Alpha.letter_padding;
  * @function
  */
 GAMEJS.Alpha.load = function() {
+	var letter_image;
 	for(var counter = 0; counter < this.alphabet.length; counter++) {
+		switch(this.alphabet[counter])
+		{
+		case "?":
+		  letter_image = "_question_mark.gif";
+		  break;
+		case "/":
+		  letter_image = "_slash.gif";
+		  break;
+		default:
+		  letter_image = this.alphabet[counter] + ".gif";
+		}		
+		
 		this.letters[this.alphabet[counter]] = new Image();
-		this.letters[this.alphabet[counter]].src = GAMEJS.gamejs_folder + "Images/" + this.alphabet[counter] + ".gif";
+		this.letters[this.alphabet[counter]].src = GAMEJS.gamejs_folder + "Images/" + letter_image;
 		_images[_images.length] = this.letters[this.alphabet[counter]];
 	}
 }
-// Movement
+// Directions
 var _up = {
 	row : -1,
 	col : 0
@@ -340,6 +353,7 @@ function _layer_class(layer_name) {
 	this.tilemaps = new Array();
 	this.lables = new Array();
 	this.textboxes = new Array();
+	this.buttons = new Array();
 
 	// Add Layer to Id Hahs
 	_id_hash[this.id] = this
@@ -359,6 +373,9 @@ function _layer_class(layer_name) {
 		}
 		for(var tbi = 0; tbi < this.textboxes.length; tbi++) {
 			this.textboxes[tbi].draw();
+		}
+		for(var bbi = 0; bbi < this.buttons.length; bbi++) {
+			this.buttons[bbi].draw();
 		}
 
 		return this;
@@ -387,12 +404,19 @@ function _layer_class(layer_name) {
 		this.lables[this.lables.length] = l;
 		return l;
 	};
-	// Create a lable
+	// Create a textbox
 	this.textbox = function(attributes) {
 		var tb = new _textbox_class(attributes);
 		tb.layer = this;
 		this.textboxes[this.textboxes.length] = tb;
 		return tb;
+	};
+	// Create a button
+	this.button = function(attributes) {
+		var button = new _button_class(attributes);
+		button.layer = this;
+		this.buttons[this.buttons.length] = button;
+		return button;
 	};
 	// Create a tilemap
 	this.tilemap = function(attributes) {
@@ -406,7 +430,7 @@ function _layer_class(layer_name) {
 }
 
 // Position Class
-function _position(row, col){
+function _position(row, col) {
 	this.row = row;
 	this.col = col;
 }
@@ -467,7 +491,17 @@ function _node_class(attributes) {
 			this.layer.screen.context.fillRect(this.x + this.layer.screen.offsetx, this.y + this.layer.screen.offsety, this.width, this.height);
 		} else if(this.type == 'image' || this.type == 'tile' || this.type == 'background') {
 			if(this.height != null && this.width != null && this.image != null) {
-				this.layer.screen.context.drawImage(this.image, this.x + this.layer.screen.offsetx, this.y + this.layer.screen.offsety, this.width, this.height);
+				var xstart = this.x + this.layer.screen.offsetx + ((this.tilemap.tilewidth - this.width) / 2 ) ;
+				var width = this.width;
+				var ystart;
+				if (this.type == 'tile' ){
+					ystart = this.y + this.layer.screen.offsety - (this.height - this.tilemap.tileheight);
+				} else {
+					ystart = this.y + this.layer.screen.offsety;
+				}
+				var height = this.height;
+				this.layer.screen.context.drawImage(this.image, xstart, ystart, width, height);
+
 			} else if(this.image != null) {
 				this.layer.screen.context.drawImage(this.image, this.x + this.layer.screen.offsetx, this.y + this.layer.screen.offsety);
 			}
@@ -479,16 +513,15 @@ function _node_class(attributes) {
 		this[attribute] = value;
 	}
 
-	this.get_position_from_direction = function(direction){
+	this.get_position_from_direction = function(direction) {
 		var y = this.row;
 		var x = this.col;
 		// Calculate New Direction
 		var newrow = y + direction.row;
 		var newcol = x + direction.col;
 
-		return new _position(newrow, newcol); 
+		return new _position(newrow, newcol);
 	}
-
 	// Movement
 	this.move = function(direction) {
 		if(this.type == 'tile') {
@@ -500,7 +533,7 @@ function _node_class(attributes) {
 
 			var pos = this.get_position_from_direction(direction);
 
-			if(!(pos.row < 0) && !(pos.col < 0) && !(pos.row >= this.tilemap.rows) && !(pos.col >= this.tilemap.cols) ) {
+			if(!(pos.row < 0) && !(pos.col < 0) && !(pos.row >= this.tilemap.rows) && !(pos.col >= this.tilemap.cols)) {
 
 				this.tilemap.move_tile(pos.row, pos.col, this);
 
@@ -518,8 +551,6 @@ function _node_class(attributes) {
 				if(!(newtilemapcol < 0) && !(newtilemapcol > (this.tilemap.cols - this.tilemap.visiblecols)) && (tilepositioncol == centercol)) {
 					this.tilemap.col = newtilemapcol;
 				}
-
-				this.tilemap.draw();
 			}
 		}
 	}
@@ -541,6 +572,7 @@ function _node_class(attributes) {
 		this.layer.screen.clickable_objects[this.layer.screen.clickable_objects.length] = this;
 		// Register function for this tiles onlick event
 		this.onclick_event = onclick_function;
+		return this;
 	}
 	// Updates the Image of the Node
 	//  src: Path to image or null if you want to remove the image
@@ -556,24 +588,179 @@ function _node_class(attributes) {
 		}
 		return this;
 	}
+
+	this.distance_to = function(tile) {
+		var distance = 0;
+
+		if(this.row > tile.row) {
+			distance = this.row - tile.row;
+		} else {
+			distance = tile.row - this.row;
+
+		}
+
+		if(this.col > tile.col) {
+			distance += this.col - tile.col;
+		} else {
+			distance += tile.col - this.col;
+
+		}
+		return distance;
+	}
+
+	this.direction_to = function(tile) {
+		var direction = null;
+		var horizontal_distance = 0;
+		var horizontal_direction = null;
+		var vertical_distance = 0;
+		var vertical_direction = null;
+
+		if(this.row == tile.row) {
+			horizontal_distance = 0;
+			horizontal_direction = null;
+		} else if(this.row > tile.row) {
+			horizontal_distance = this.row - tile.row;
+			horizontal_direction = _up;
+		} else {
+			horizontal_distance = tile.row - this.row;
+			horizontal_direction = _down;
+		}
+
+		if(this.col == tile.col) {
+			vertical_distance = 0;
+			vertical_direction = null;
+		} else if(this.col > tile.col) {
+			vertical_distance = this.col - tile.col;
+			vertical_direction = _left
+		} else {
+			vertical_distance = tile.col - this.col;
+			vertical_direction = _right
+		}
+
+		if(vertical_distance == horizontal_distance) {
+			// Should be random
+			switch(Math.floor(Math.random() * 2)) {
+				case 0:
+					direction = vertical_direction;
+					break;
+				case 1:
+					direction = horizontal_direction;
+					break;
+			}
+
+		} else if(vertical_distance > horizontal_distance) {
+			direction = vertical_direction;
+		} else {
+			direction = horizontal_direction;
+		}
+
+		return direction;
+	}
 }
 
 function _log(msg) {
 	//
 }
 
+// ENUM VALUES FOR DIALOGS
+// TEXT Displays Text and a OK button to close the frame
+// input : should be a string
+GAMEJS.TEXT = 1;
+// ASK Displays Text and
+GAMEJS.ASK = 2;
+GAMEJS.DISPLAY = 3;
+GAMEJS.CUSTOM = 4;
+
+function _dialog_class(attributes) {
+	this.canvas = (attributes.canvas || _visible_screen.canvas);
+	this.screen_name = (attributes.screen_name || "dialog");
+	this.width = (attributes.width || this.canvas.width - 40);
+	this.height = (attributes.height || this.canvas.height - 40);
+
+	this.screen = _screen(_visible_screen.canvas_name, {
+		offsetx : (20),
+		offsety : (20),
+		width : this.width,
+		height : this.height,
+		name : this.screen_name,
+	});
+
+	// Create Default Layer
+	// This can be overrided with function 'override_layer'
+	this.layer = this.screen.layer("background");
+	this.layer.node({
+		image : GAMEJS.gamejs_folder + "Images/dialog_background.png",
+		width : width,
+		height : height,
+	});
+
+	// Override Layer
+	this.override_layer = function(layer) {
+		this.default_layer = layer;
+	}
+
+	this.button = function(x, y, text, callback) {
+		this.layer.button({
+			x : x,
+			y : y,
+			text : text
+		}).onclick(callback);
+	}
+
+	this.center = function() {
+		return (this.width / 2);
+	}
+	return this;
+}
+
+function Dialog(type, input, attributes) {
+
+	if(attributes == null) {
+		attributes = {};
+	}
+
+	// Create Dialog
+	var dialog = _dialog_class(attributes);
+
+	if(type == GAMEJS.TEXT) {
+		// Create Text
+		dialog.layer.textbox({
+			x : 10,
+			y : 10,
+			cols : (dialog.width - 30) / GAMEJS.Alpha.letter_width,
+			rows : (dialog.height - 50) / GAMEJS.Alpha.letter_height,
+			padding : 5,
+			bordersize : 1,
+			text : input
+		});
+
+		// Create Button
+		dialog.button(dialog.width - 55, dialog.height - 25, "Close", function() {
+			this.layer.screen.close();
+		});
+	}
+
+	// Draw screen and return dialog
+	dialog.screen.draw();
+	return dialog;
+}
+
 function _screen_class(canvas_name, attributes) {
 	// Attributes
 	this.id = _generate_id("s");
-	this.name = "Screen";
+	this.name = (attributes.name || "Screen");
 	this.layers = new Array();
 	this.canvas = document.getElementById(canvas_name);
+	this.canvas_name = canvas_name;
 	this.context = this.canvas.getContext("2d");
 	this.keypresses = {};
 	this.clickable_objects = new Array();
+	this.previous_screen = null;
 
 	this.offsetx = (attributes.offsetx || 0);
 	this.offsety = (attributes.offsety || 0);
+	this.width = (attributes.width || this.canvas.width);
+	this.height = (attributes.height || this.canvas.height);
 
 	// Add screen to Object Hash
 	_id_hash[this.id] = this
@@ -590,12 +777,13 @@ function _screen_class(canvas_name, attributes) {
 	// Draws all the visible layers of the screen
 	// Returns self-reference
 	this.draw = function() {
-		this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+		this.context.clearRect(this.offsetx, this.offsety, this.width, this.height);
 		for(var li = 0; li < this.layers.length; li++) {
 			if(this.layers[li].visible == true) {
 				this.layers[li].draw();
 			}
 		}
+		this.previous_screen = _visible_screen;
 		_visible_screen = this;
 		return this;
 	};
@@ -606,6 +794,11 @@ function _screen_class(canvas_name, attributes) {
 		this.keypresses[charUpperCase] = func;
 		return this;
 	};
+
+	this.close = function() {
+		_visible_screen = this.previous_screen;
+		_visible_screen.draw();
+	}
 }
 
 function _lable_class(attributes) {
@@ -626,14 +819,41 @@ function _lable_class(attributes) {
 	}
 }
 
+function _button_class(attributes) {
+	var button = new _textbox_class(attributes);
+	button.bordersize = (attributes.bordersize || 1);
+	button.padding = (attributes.padding || 5);
+	button.color = (attributes.color || '#444');
+
+	button.onclick = function(onclick_function) {
+		this.layer.screen.clickable_objects[this.layer.screen.clickable_objects.length] = this;
+		// Register function for this tiles onlick event
+		this.onclick_event = onclick_function;
+		return this;
+	}
+	return button;
+}
+
 function _textbox_class(attributes) {
 	this.padding = (attributes.padding || 3);
 	this.x = (attributes.x || 0);
 	this.y = (attributes.y || 0);
 	this.text = (attributes.text || "");
-	this.width = (attributes.width || 0);
+	this.cols = (attributes.cols || this.text.length || 1);
 	this.color = (attributes.color || '#000');
-	this.height = (attributes.height || 0);
+	this.rows = (attributes.rows || 1);
+
+	this.width = (attributes.width || null);
+	this.height = (attributes.height || null);
+	this.bordersize = (attributes.bordersize || 0);
+	this.bordercolor = (attributes.bordercolor || '#FFF');
+	this.image = null;
+
+	// Load Image
+	if (attributes.image != null){
+		this.image = new Image();
+		this.image.src = attributes.image;
+	}
 
 	this.draw = function() {
 		var x = this.x + this.layer.screen.offsetx;
@@ -645,15 +865,29 @@ function _textbox_class(attributes) {
 		var character = null;
 		var img = null;
 		var word = null;
-		width = (this.width * GAMEJS.Alpha.letter_width) + (this.padding * 2) - GAMEJS.Alpha.letter_padding;
-		height = (this.height * GAMEJS.Alpha.letter_height) + (this.padding * 2) - GAMEJS.Alpha.letter_padding;
+		this.width = (this.width || ((this.cols * GAMEJS.Alpha.letter_width) + (this.padding * 2) - GAMEJS.Alpha.letter_padding));
+		this.height = (this.height || ((this.rows * GAMEJS.Alpha.letter_height) + (this.padding * 2) - GAMEJS.Alpha.letter_padding));
 
-		this.layer.screen.context.fillStyle = this.color;
-		this.layer.screen.context.fillRect(x, y, width, height);
+		if(this.bordersize > 0) {
+			this.layer.screen.context.fillStyle = this.bordercolor;
+			this.layer.screen.context.fillRect(x, y, this.width, this.height);
+			this.layer.screen.context.fillStyle = this.color;
+			this.layer.screen.context.fillRect(x + this.bordersize, y + this.bordersize, this.width - (this.bordersize * 2), this.height - (this.bordersize * 2));
+		} else {
+			this.layer.screen.context.fillStyle = this.color;
+			this.layer.screen.context.fillRect(x, y, this.width, this.height);
+		}
+		
+		// Draw background image
+		if (this.image != null){
+			this.layer.screen.context.drawImage(this.image, this.x + this.layer.screen.offsetx, this.y + this.layer.screen.offsety, this.width, this.height);
+		}
 
 		if(this.text != null && this.text != undefined) {
 
-			var words = this.text.toString().split(" ");
+			var temp_text = this.text.replace(/\n/g, " \n ");
+			var words = temp_text.toString().split(/[ ]/);
+
 			for(var wi = 0; wi < words.length; wi++) {
 				word = words[wi];
 
@@ -661,15 +895,26 @@ function _textbox_class(attributes) {
 				if(word[0] == "{") {
 					var parameter_name = "";
 					var temp_counter = 1;
-					while(word[temp_counter] != "}") {
+					while(word[temp_counter] != "}" && temp_counter < word.length) {
 						parameter_name = parameter_name + word[temp_counter];
 						temp_counter = temp_counter + 1;
 					}
-					word = word.replace("{" + parameter_name + "}", eval(parameter_name));
+					try {
+					  var evaluated_value = eval(parameter_name);
+					}
+					catch(err){
+					   _log(err);
+					}
+					if ( evaluated_value === undefined){
+						_log("Could not evaluate parameter" + word );
+						word = "";
+					} else {
+						word = "" + evaluated_value;
+					}
 				}
 
 				// Check if we want to move to next row
-				if(word.length + col > this.width) {
+				if(word.length + col > this.cols) {
 					row = row + 1;
 					col = 0;
 				}
@@ -690,14 +935,19 @@ function _textbox_class(attributes) {
 							img = GAMEJS.Alpha.letters[character];
 							if(img != null) {
 								this.layer.screen.context.drawImage(img, imgx, imgy);
+								//Move To Next Letter
 								col = col + 1;
 							} else {
-								_log('Trided to write character ' + character + ' which is not supported.');
+								_log('Tried to write character ' + character + ' which is not supported.');
 							}
 							break;
 					}
 				}
-				col = col + 1;
+				// Move A Space Between Each Words
+				// But don't do this on the first column, because this is after a line break
+				if(col != 0) {
+					col = col + 1;
+				}
 			}
 		}
 	}
@@ -735,8 +985,15 @@ function _tilemap_class(attributes) {
 	if(attributes.rows != null) {
 		this.rows = attributes.rows;
 	}
+	if(attributes.tilewidth != null) {
+		this.tilewidth = attributes.tilewidth;
+	}
+	if(attributes.tileheight != null) {
+		this.tileheight = attributes.tileheight;
+	}
 	if(attributes.tilesize != null) {
-		this.tilesize = attributes.tilesize;
+		this.tilewidth = attributes.tilesize;
+		this.tileheight = attributes.tilesize;
 	}
 	if(attributes.x != null) {
 		this.x = attributes.x;
@@ -810,10 +1067,21 @@ function _tilemap_class(attributes) {
 		this.tiles[row][col] = tile;
 		return tile;
 	}
-	// Places a specified tile on the tilemap
+	// Places a specified tile ( NOT A COPY OF IT ) on the tilemap
+	this.put_tile = function(row, col, tile) {
+		tile.layer = this.layer;
+		tile.row = row;
+		tile.col = col;
+		tile.tilemap = this;
+		this.tiles[row][col] = tile;
+		return tile;
+	}
+	// Places a copy of a specified tile on the tilemap
 	this.place_tile = function(row, col, original_tile) {
 		var tile = jQuery.extend(true, {}, original_tile);
+		tile.layer = this.layer;
 		tile.id = _generate_id("n");
+		// Why are we doing this?
 		original_tile.row = row;
 		original_tile.col = col;
 		tile.row = row;
@@ -821,6 +1089,7 @@ function _tilemap_class(attributes) {
 		tile.tilemap = this;
 		this.tiles[row][col] = tile;
 		return tile;
+
 	}
 	// Create A tile object ( but does not place it on the map )
 	this._tile = function(attributes) {
@@ -890,18 +1159,15 @@ function _tilemap_class(attributes) {
 	this.draw = function() {
 		for(var row = this.row; row < (this.visiblerows + this.row); row++) {
 			for(var col = this.col; col < (this.visiblecols + this.col); col++) {
+
+				this.backgrounds[row][col].x = ((col - this.col) * this.tilewidth) + this.x;
+				this.backgrounds[row][col].y = ((row - this.row) * this.tileheight) + this.y;
+				this.backgrounds[row][col].draw();
+
 				if(this.tiles[row][col] != null) {
-					this.tiles[row][col].x = ((col - this.col) * this.tilesize) + this.x;
-					this.tiles[row][col].y = ((row - this.row) * this.tilesize) + this.y;
-					this.tiles[row][col].width = this.tilesize;
-					this.tiles[row][col].height = this.tilesize;
+					this.tiles[row][col].x = ((col - this.col) * this.tilewidth) + this.x;
+					this.tiles[row][col].y = ((row - this.row) * this.tileheight) + this.y;
 					this.tiles[row][col].draw();
-				} else if(this.backgrounds[row][col] != null) {
-					this.backgrounds[row][col].x = ((col - this.col) * this.tilesize) + this.x;
-					this.backgrounds[row][col].y = ((row - this.row) * this.tilesize) + this.y;
-					this.backgrounds[row][col].width = this.tilesize;
-					this.backgrounds[row][col].height = this.tilesize;
-					this.backgrounds[row][col].draw();
 				}
 			}
 		}
@@ -923,6 +1189,27 @@ function _tilemap_class(attributes) {
 	this.get_tile = function(i, j) {
 		return this.tiles[i][j];
 	}
+	
+	this.dump = function(){
+		var dump = "";
+		dump += "id:"+ this.id +",";
+		dump += "cols:"+this.cols+",";
+		dump += "rows:"+this.rows+",";
+		dump += "visiblecols:"+this.visiblecols+",";
+		dump += "visiblerows:"+this.visiblerows+",";
+		dump += "col:"+this.col+",";
+		dump += "row:"+this.row+",";
+		dump += "tilewidth:"+this.tilewidth+",";
+		dump += "tileheight:"+this.tileheight+",";
+		dump += "x:"+this.x+",";
+		dump += "y:"+this.y+",";
+		
+		var backgrounds = "";
+		var tiles = "";
+		
+		return "{"+ dump +" backgrounds:{"+ backgrounds +"},tiles:{"+ tiles +"}}";
+		
+	}
 }
 
 // Builders
@@ -931,6 +1218,18 @@ function _screen(screen_name, attributes) {
 	var s = new _screen_class(screen_name, attributes)
 
 	return s;
+}
+
+function _tile(attributes) {
+	var tile = new _node_class(attributes);
+	//tile.layer = this.layer;
+	tile.type = 'tile';
+	// Update Interfaces
+	for(var i = 0; i < tile.interfaces.length; i++) {
+		tile.interfaces[i] = jQuery.extend(true, {}, tile.interfaces[i]);
+		tile.interfaces[i].parent = tile;
+	}
+	return tile;
 }
 
 // Search Functions
@@ -971,7 +1270,9 @@ function _load(screen) {
 
 			for(var coi = 0; coi < _visible_screen.clickable_objects.length; coi++) {
 				var object = _visible_screen.clickable_objects[coi];
-				if(object.x <= x && (object.x + object.width) >= x && object.y <= y && (object.y + object.height) >= y) {
+				var obj_x = (_visible_screen.offsetx + object.x);
+				var obj_y = (_visible_screen.offsety + object.y);
+				if(obj_x <= x && (obj_x + object.width) >= x && obj_y <= y && (obj_y + object.height) >= y) {
 					object.onclick_event();
 				}
 			}
